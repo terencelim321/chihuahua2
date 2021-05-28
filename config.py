009711c3788013f45427ef9000c58c2b351a4c4e
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-28 11:03:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['postgres://ooxeoyhmzkoaet:5d0156ba0cd2041fcce04a1af768b93c5834e877d9086390d7c2111dd608f8cf@ec2-34-200-94-86.compute-1.amazonaws.com:5432/ddfcrdjbfcqmlo']
 
 class ProductionConfig(Config):
     DEBUG = False
