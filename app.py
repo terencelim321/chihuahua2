396206260af6cde145769a4dc976db5b47897fcb
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-28 12:47:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from flask import Flask, render_template, request, json, jsonify
 app = Flask(__name__)
 
 #start app route which is /
-@app.route("/sum")
+@app.route("/sum ", methods=['GET', 'POST'])
 #takes json info from php file
 def main():
-        with open("app.php","rb") as f:
-            jsonData = json.loads(f.read())
+        #with open("app.php","rb") as f:
+        #    jsonData = json.loads(f.read())
 
-        for key,value in jsonData['numbers'].items():
-            if key == 'number1':
-                x = value
-            else:
-                y = value
+        #for key,value in jsonData['numbers'].items():
+        #    if key == 'number1':
+        #        x = value
+        #    else:
+        #        y = value
+        if request.method == 'POST':
+            num1 = request.form['num1']
+            num2 = request.form['num2']
 
-        sum = str(x + y)
+        sum = num1 + num2 
 
         return sum
 
